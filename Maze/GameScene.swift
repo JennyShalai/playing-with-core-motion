7ee9561c61e42d8acd72a27e6ca4072c3608d7f3
JennyShalai <jenny.shalai@gmail.com>
2016-07-27 23:01:19 -0400

    walls, pokemon and ball are added

--- Maze/GameScene.swift
+++ Maze/GameScene.swift
@@ -9,20 +9,22 @@
 import SpriteKit
 import CoreMotion
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     let manager = CMMotionManager()
+    var ball = SKSpriteNode()
+    var vaporeon = SKSpriteNode()
     
     override func didMoveToView(view: SKView) {
+        self.physicsWorld.contactDelegate = self
         
+        ball = self.childNodeWithName("ball") as! SKSpriteNode
+        vaporeon = self.childNodeWithName("vaporeon") as! SKSpriteNode
+    
         manager.startAccelerometerUpdates()
-        
         manager.accelerometerUpdateInterval = 0.5
-        
         manager.startAccelerometerUpdatesToQueue(NSOperationQueue.mainQueue()) {
-            
             (data, error) in
-            
             self.physicsWorld.gravity = CGVectorMake(CGFloat((data?.acceleration.x)!) * 30,
                                                      CGFloat((data?.acceleration.y)!) * 30)
             print("X: \((data?.acceleration.x)!)")
@@ -32,6 +34,16 @@ class GameScene: SKScene {
         
     }
    
+    func didBeginContact(contact: SKPhysicsContact) {
+        var ball = contact.bodyA
+        var vaporeon = contact.bodyB
+        
+        if ball.categoryBitMask == 1 && vaporeon.categoryBitMask == 2 || ball.categoryBitMask == 2 && vaporeon.categoryBitMask == 1 {
+            // you won
+            print("you won!")
+        }
+    }
+    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
